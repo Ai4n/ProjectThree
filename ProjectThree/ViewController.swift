73954947b06269e0cb064693e32c6caa55caf188
Akhan Beisenbayev <akhan.beis@gmail.com>
2020-09-01 16:48:50 +0600

    New project

---
+++ ProjectThree/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProjectThree
+//
+//  Created by Mac OS on 9/1/20.
+//  Copyright © 2020 Mac OS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
