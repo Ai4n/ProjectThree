3d965c22a09342142c125b4ecf12728a9298d13e
Akhan Beisenbayev <akhan.beis@gmail.com>
2020-09-01 18:54:50 +0600

    TableView example

---
+++ ProjectThree/ToDoCell.swift
@@ -0,0 +1,16 @@
+//
+//  ToDoCell.swift
+//  ProjectThree
+//
+//  Created by Mac OS on 9/1/20.
+//  Copyright © 2020 Mac OS. All rights reserved.
+//
+
+import UIKit
+
+class ToDoCell: UITableViewCell {
+
+    @IBOutlet weak var titleLable: UILabel!
+    @IBOutlet weak var imageRight: UIImageView!
+    
+}
--- ProjectThree/ViewController.swift
+++ ProjectThree/ViewController.swift
@@ -10,11 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    let array: [String] = ["day","priority", "today", "in process", "done", "day","priority", "today", "in process", "done", "day","priority", "today", "in process", "done", "day","priority", "today", "in process", "done", "day","priority", "today", "in process", "done", "day","priority", "today", "in process", "done", "day","priority", "today", "in process", "done", "day","priority", "today", "in process", "done", "day","priority", "today", "in process", "done", "day","priority", "today", "in process", "done", "day","priority", "today", "in process", "done", "day","priority", "today", "in process", "done", "day","priority", "today", "in process", "done", "day","priority", "today", "in process", "done", "day","priority", "today", "in process", "done", "day","priority", "today", "in process", "done", "day","priority", "today", "in process", "done", "day","priority", "today", "in process", "done", "day","priority", "today", "in process", "done", "day","priority", "today", "in process", "done", "day","priority", "today", "in process", "done", "day","priority", "today", "in process", "done", "day","priority", "today", "in process", "done", "day","priority", "today", "in process", "done", "day","priority", "today", "in process", "done", "day","priority", "today", "in process", "done", "day","priority", "today", "in process", "done", "day","priority", "today", "in process", "done", "day","priority", "today", "in process", "done", "day","priority", "today", "in process", "done", "day","priority", "today", "in process", "done", "day","priority", "today", "in process", "done", "day","priority", "today", "in process", "done", "day","priority", "today", "in process", "done", "day","priority", "today", "in process", "done", "day","priority", "today", "in process", "done", "day","priority", "today", "in process", "done", "day","priority", "today", "in process", "done", "day","priority", "today", "in process", "done", "day","priority", "today", "in process", "done", "day","priority", "today", "in process", "done", "day","priority", "today", "in process", "done", "day","priority", "today", "in process", "done", "day","priority", "today", "in process", "done", "day","priority", "today", "in process", "done", "day","priority", "today", "in process", "done", "day","priority", "today", "in process", "done", "day","priority", "today", "in process", "done", "day","priority", "today", "in process", "done", "day","priority", "today", "in process", "done", "day","priority", "today", "in process", "done", "day","priority", "today", "in process", "done", "day","priority", "today", "in process", "done", "day","priority", "today", "in process", "done", "day","priority", "today", "in process", "done", "day","priority", "today", "in process", "done", "day","priority", "today", "in process", "done", "day","priority", "today", "in process", "done", "day","priority", "today", "in process", "done", "day","priority", "today", "in process", "done", "day","priority", "today", "in process", "done", "day","priority", "today", "in process", "done", "day","priority", "today", "in process", "done", "day","priority", "today", "in process", "done", "day","priority", "today", "in process", "done"]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.dataSource = self
+       // tableView.register(ToDoCell.self, forCellReuseIdentifier: "ToDo")
         // Do any additional setup after loading the view.
     }
 
 
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        array.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDo", for: indexPath) as! ToDoCell
+        let title = array[indexPath.row]
+        cell.titleLable?.text = title
+        return cell
+    }
+    
+    
+}
